b5d85fcc104de4758acb0206befc90af67aa42f6
Piotrek Okoński <piotrek@okonski.org>
2011-02-05 18:39:55 +0100

    Animowany menu

--- js/site.js
+++ js/site.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
-  for(i=0;i< 25;i++){
-    $('#main, #quickbar').append('<br/>');
-  }
+  $('ul[sub]').prepend('<li><a class="back" href="javascript:void(0)">&laquo; Back</a></li>');
+  $('.clickable').each(function(){
+    $(this).click(showSubMenu);
+    $(this).append(" &raquo;");
+  });
+  $('.sub .back').click(returnToParentMenu);
 });
+
+function showSubMenu(){
+  $(this).parents('ul').hide('slide', { direction: 'up' }, 200);
+  $('ul.sub[sub="'+$(this).attr('sub')+'"]').delay(200).show('slide', { direction: 'left' }, 200);
+}
+
+function returnToParentMenu(){
+  $(this).parents('ul').hide('slide', { direction: 'up' }, 200);
+  $('a[sub="'+$(this).parents('ul').attr('sub')+'"]').parents('ul').delay(200).show('slide', { direction: 'left' }, 200);
+}
